96e528638add45a6c59914a9c961f607a03941ea
Kevin Rejko <krejko@gmail.com>
2019-11-03 10:01:10 -0700

    Added about sectoin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
