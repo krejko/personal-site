26676c9ddc8613fb7c09d17eacc35a513b364550
Kevin Rejko <krejko@gmail.com>
2019-11-03 17:52:37 -0700

    Added initial layouts for side projects section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
+import { SideProjectsComponent } from './side-projects/side-projects.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { OrganizationsComponent } from './organizations/organizations.component'
     HeroComponent,
     AboutComponent,
     ProjectsComponent,
-    OrganizationsComponent
+    OrganizationsComponent,
+    SideProjectsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/side-projects/side-projects.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'side-projects',
+  templateUrl: './side-projects.component.html',
+  styleUrls: ['./side-projects.component.scss']
+})
+export class SideProjectsComponent implements OnInit {
+
+
+  projects = [
+    {
+      name: "Neato Keen Productions",
+      dates: "",
+      circleLogo: "./assets/images/side-projects/nkp-logo-circle.png",
+      textLogo: "./assets/images/side-projects/nkp-logo-text.png",
+      description: "NeatoKeen Productions is a ...",
+      link: "https://nkp.io"
+    },
+    {
+      name: "TADTOY",
+      dates: "",
+      circleLogo: "./assets/images/side-projects/tadtoy-logo-circle.jpeg",
+      textLogo: "./assets/images/side-projects/tadtoy-logo-text.png",
+      description: "TADTOY is a social media platform focused on...",
+      link: "http://tadtoy.com"
+    },
+    {
+      name: "₮ЯᗑƆ₭Ƨ",
+      dates: "",
+      circleLogo: "./assets/images/side-projects/tracks-logo-circle.png",
+      textLogo: "./assets/images/side-projects/tracks-logo-text.png",
+      description: "TRACKS Music is where ...",
+      link: "https://soundcloud.com/tracks-music"
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
