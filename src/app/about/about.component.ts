b4ed3f236ed1947c301b924f7c54014c5951a0c4
Kevin Rejko <krejko@gmail.com>
2019-11-20 10:10:19 -0700

    Added ability to laod resume from google spreadsheet

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResumeService } from '../services/resume.service';
 
 @Component({
   selector: 'about',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private resumeService: ResumeService
+  ) { }
 
   ngOnInit() {
   }
---
+++ src/app/services/resume.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import * as d3 from "d3"
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResumeService {
+
+  isLoading = false
+
+  resumeURLs: Array<string> = [
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSExAuVSyBExZ5gBD2-vvVPcF5YGsm50qy2qzTVlAGwSpiwxPd80lMnIXyZW_pcJKCZyHhBlZyyhRT0/pub?gid=1837361825&single=true&output=csv",
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSExAuVSyBExZ5gBD2-vvVPcF5YGsm50qy2qzTVlAGwSpiwxPd80lMnIXyZW_pcJKCZyHhBlZyyhRT0/pub?gid=1847563364&single=true&output=csv",
+  ]
+
+  resume = {
+    about: undefined,
+    experience: undefined
+  }
+
+  constructor() {
+    this.isLoading = true
+    this.loadCSVURLs(this.resumeURLs, (values, error) => {
+      this.isLoading = false
+      console.log("Got values", values)
+
+
+      this.resume = values.reduce((acc, val, idx) => {
+        if (idx === 0) { // About
+          acc.about = this.arrayToDict(val, "key", "value")['about']
+        } else if (idx === 1) { // Experience
+          acc.experience = val
+        }
+        return acc
+      }, this.resume)
+      console.log("Got resume", this.resume)
+
+    }, (error) => {
+      console.warn(error)
+    })
+  }
+
+  loadCSVURLs(urls, callback, error) {
+    Promise.all(urls.map(url => d3.csv(url))).then(callback).catch(error)
+  }
+
+  arrayToDict(array, key, value?) {
+    return array.reduce((acc, el) => {
+      acc[el[key]] = value ? el[value] : el
+      return acc
+    }, {})
+  }
+}
